da126e87d3679411923a68757b627aa77c6ef8cc
enderneko <enderneko-dev@outlook.com>
2024-08-01 11:35:45 +0800

    强制校验时间，并提升版本号

--- Utils/WoWTokenUtils.lua
+++ Utils/WoWTokenUtils.lua
@@ -4,61 +4,74 @@ BigFootBot.token = {}
 local T = BigFootBot.token
 local U = BigFootBot.utils
 
+local token_debug_print = false
 local TIME_POINTS = {15, 35, 55} -- 获取数据的时间点
-local UPDATE_INTERVAL = (TIME_POINTS[2] - TIME_POINTS[1]) * 60 -- 更新间隔（秒）
+
+---------------------------------------------------------------------
+-- 下次执行的倒计时
+---------------------------------------------------------------------
+local function GetNextUpdateCountdown()
+    local t = date("*t", GetServerTime())
+    if t.min >= TIME_POINTS[#TIME_POINTS] then
+        -- 大于最后一个时间点
+        return (60 - t.min) * 60 - t.sec + TIME_POINTS[1] * 60, t
+    else
+        for _, v in pairs(TIME_POINTS) do
+            if t.min < v then
+                return (v - t.min) * 60 - t.sec, t
+            end
+        end
+    end
+end
 
 ---------------------------------------------------------------------
 -- 保存数据
 ---------------------------------------------------------------------
+local timestamp
+
 local function SaveTokenPrice()
     local price = C_WowTokenPublic.GetCurrentMarketPrice()
-    local t = date("*t", GetServerTime())
-    -- print("UPDATE:", GetServerTime(), string.format("%02d:%02d:%02d", t.hour, t.min, t.sec), price)
-    t.sec = 0
-    BigFootBotTokenDB[time(t)] = price
+    if token_debug_print then
+        print("UPDATE:", "[" .. timestamp .. "] = " .. price)
+    end
+    BigFootBotTokenDB[timestamp] = price
+    timestamp = nil
 end
 
 local function RequestTokenPrice()
-    if InCombatLockdown() then return end -- 非战斗中
-    -- print("UPDATE IN 10 SEC!!!!!")
-    C_WowTokenPublic.UpdateMarketPrice() -- 请求数据
-    C_Timer.After(10, SaveTokenPrice) -- 10秒后记录，而非监听 TOKEN_MARKET_PRICE_UPDATED 事件
-end
+    -- 校验时间
+    local t = date("*t", GetServerTime())
+    for _, v in pairs(TIME_POINTS) do
+        if t.min == v then
+            t.sec = 0
+            timestamp = time(t)
+            break
+        end
+    end
 
----------------------------------------------------------------------
--- 立即开始
----------------------------------------------------------------------
-local function StartNow()
-    RequestTokenPrice() -- 立即保存
-    C_Timer.NewTicker(UPDATE_INTERVAL, RequestTokenPrice) -- 定时器
-end
+    if timestamp and not InCombatLockdown() then -- 非战斗中
+        if token_debug_print then
+            print("UPDATE IN 10 SEC!!! ", string.format("%02d:%02d:%02d", t.hour, t.min, t.sec))
+        end
+        C_WowTokenPublic.UpdateMarketPrice() -- 请求数据
+        C_Timer.After(10, SaveTokenPrice) -- 10秒后记录，而非监听 TOKEN_MARKET_PRICE_UPDATED 事件
+    end
 
----------------------------------------------------------------------
--- 延迟到下个 0/30 开始
----------------------------------------------------------------------
-local function StartDelayed(timeDelayed, t)
-    -- print("NOW:", GetServerTime(), string.format("%02d:%02d:%02d", t.hour, t.min, t.sec), ", DELAYED:", string.format("%dm%ds", floor(timeDelayed / 60), timeDelayed % 60))
-    C_Timer.After(timeDelayed, StartNow)
+    -- 准备下次
+    local timeDelayed, t = GetNextUpdateCountdown()
+    if token_debug_print then
+        print("NOW:", time(t), string.format("%02d:%02d:%02d", t.hour, t.min, t.sec), ", NEXT:", string.format("%dm%ds", floor(timeDelayed / 60), timeDelayed % 60))
+    end
+    C_Timer.After(GetNextUpdateCountdown(), RequestTokenPrice)
 end
 
 ---------------------------------------------------------------------
 -- 启用
 ---------------------------------------------------------------------
 function T:StartTockenPriceUpdater()
-    local t = date("*t", GetServerTime())
-
-    if t.min > TIME_POINTS[#TIME_POINTS] then
-        -- 大于最后一个时间点
-        StartDelayed((60 - t.min) * 60 - t.sec + TIME_POINTS[1] * 60, t)
-    else
-        for _, v in pairs(TIME_POINTS) do
-            -- if t.min == v then
-            --     StartNow()
-            --     break
-            if t.min < v then
-                StartDelayed((v - t.min) * 60 - t.sec, t)
-                break
-            end
-        end
+    local timeDelayed, t = GetNextUpdateCountdown()
+    if token_debug_print then
+        print("NOW:", time(t), string.format("%02d:%02d:%02d", t.hour, t.min, t.sec), ", COUNTDOWN:", string.format("%dm%ds", floor(timeDelayed / 60), timeDelayed % 60))
     end
+    C_Timer.After(timeDelayed, RequestTokenPrice)
 end
