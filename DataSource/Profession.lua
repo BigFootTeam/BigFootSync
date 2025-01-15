da4586010fbb943daca695856a96d61ebfe07d6a
enderneko <enderneko-dev@outlook.com>
2025-01-15 13:52:22 +0800

    更新 profNames

--- DataSource/Profession.lua
+++ DataSource/Profession.lua
@@ -12,21 +12,21 @@ local GetProfessionInfo = GetProfessionInfo
 -- profession names
 ---------------------------------------------------------------------
 local porfNames = {
-    [129] = "first_aid",
-    [164] = "blacksmithing",
-    [165] = "leatherworking",
-    [171] = "alchemy",
-    [182] = "herbalism",
-    [184] = "cooking",
-    [186] = "mining",
-    [197] = "tailoring",
-    [202] = "engineering",
-    [333] = "enchanting",
-    [356] = "fishing",
-    [393] = "skinning",
-    [755] = "jewelcrafting",
-    [773] = "inscription",
-    [794] = "archaeology"
+    -- [129] = "first_aid",
+    [164] = "blacksmithing", -- 锻造
+    [165] = "leatherworking", -- 制皮
+    [171] = "alchemy", -- 炼金术
+    [182] = "herbalism", -- 草药学
+    -- [184] = "cooking",
+    [186] = "mining", -- 采矿
+    [197] = "tailoring", -- 裁缝
+    [202] = "engineering", -- 工程学
+    [333] = "enchanting", -- 附魔
+    -- [356] = "fishing",
+    [393] = "skinning", -- 剥皮
+    [755] = "jewelcrafting", -- 珠宝加工
+    [773] = "inscription", -- 铭文
+    -- [794] = "archaeology"
 }
 
 ---------------------------------------------------------------------
